c87997480b3848081cd785d2cf819dd824ba7e8e
Maira Bello <maira.araujo@liferay.com>
2014-08-08 10:19:53 -0300

    Add SoyMsgExtractor.jar to npm package

--- build-package.sh
+++ build-package.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 ant generated-soyutils SoyToJsSrcCompiler &&
+ant generated-soyutils SoyMsgExtractor &&
 cp build/SoyToJsSrcCompiler.jar package &&
+cp build/SoyMsgExtractor.jar package &&
 cp build/javascript/* package
